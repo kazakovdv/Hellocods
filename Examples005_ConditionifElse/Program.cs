3001031dd2fcd172fe5949252f77c88ec0422afa
Danil <kazakovdv1@gmail.com>
2022-08-20 23:31:33 +0500

    Examples 005 Condition if-Else

---
+++ Examples005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "данил")
+{
+    Console.WriteLine("Ура, это же Данил!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
